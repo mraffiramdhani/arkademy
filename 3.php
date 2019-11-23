fe46cb364544bebe4d402b348102e484f045a92b
mraffiramdhani <raffi.programming@gmail.com>
2019-11-23 12:52:07 +0700

    Update Soal No.3

---
+++ 3.php
@@ -0,0 +1,30 @@
+<?php  
+
+function printString(int $param)
+{
+	$arr = array();
+	for($i = 1;$i <= $param;$i++)
+	{
+		if ($i % 3 == 0 && $i % 7 == 0) {
+			array_push($arr, 'Arkademy');
+		}
+		else{
+			
+			if($i%3 == 0){
+				array_push($arr, 'Arka');
+			}
+			elseif ($i%7 == 0) {
+				array_push($arr, 'Demy');
+			}
+			else{
+				array_push($arr, $i);
+			}
+		}
+	}
+
+	return implode(', ', $arr);
+}
+
+echo printString(21);
+
+?>
